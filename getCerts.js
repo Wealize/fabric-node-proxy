a344e127430a1113482794dc5852983e07be27bb
nieves <nieves@nievess-MacBook-Pro.local>
2018-11-07 14:05:45 +0100

    sync

--- getCerts.js
+++ getCerts.js
@@ -2,7 +2,7 @@
 // Mirar si existe un admin en S3 tnp-node-proxy bucket carpeta hfc-key-store
 var S3Service = require('./services/S3Service').default;
 var s3_service = new S3Service();
-console.log(s3_service.get('admin'));
+s3_service.syncDirectory();
 // Si no existe el admin nos vamos
 // Leer y traernos el archivo admin
 // Leer y traernos el cert privado y publico de admin
--- services/S3Service.js
+++ services/S3Service.js
@@ -4,8 +4,8 @@ var config = require('../config');
 export default class S3Service {
     constructor() {
         const awsS3Client = new AWS.S3({
-            accessKeyId: config.accessKeyId, //process.ENV.AWS_ACCESS_KEY_ID,
-            secretAccessKey: config.secretAccessKey, //process.ENV.AWS_ACCESS_SECRET_ID,
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID || config.accessKeyId,
+            secretAccessKey: process.env.AWS_ACCESS_SECRET_ID || config.secretAccessKey,
             region: 'eu-central-1'
           });
         this.client = s3.createClient({
@@ -13,25 +13,36 @@ export default class S3Service {
         });
     }
 
-    get(user) {
+    download(user) {
         var params = {
+            localFile: "hfc-key-store/"+user,
             s3Params: {
-                Bucket: config.bucket,// process.ENV.BUCKET_NAME
-              }
-        }
-        var keys = this.client.listObjects(params);
-        return keys;
+              Bucket: process.env.BUCKET_NAME || config.bucket,
+              Key: user,
+            },
+          };
+          var downloader = this.client.downloadFile(params);
+
+          downloader.on('error', function(err) {
+            console.error("unable to download:", err.stack);
+          });
+          downloader.on('progress', function() {
+            console.log("progress", downloader.progressAmount, downloader.progressTotal);
+          });
+          downloader.on('end', function() {
+            console.log("done downloading");
+          });
+
     }
 
-    syncDirectory(){ // it uploads file AWS
+    syncDirectory(){
         var params = {
             localDir: "hfc-key-store",
-            deleteRemoved: true,
+            // deleteRemoved: true,
             s3Params: {
-              Bucket: config.bucket //process.ENV.BUCKET_NAME,
+              Bucket: process.env.BUCKET_NAME || config.bucket
             }
           };
-          console.log(params);
           var uploader = this.client.uploadDir(params);
           uploader.on('error', function(err) {
             console.error("unable to sync:", err.stack);
