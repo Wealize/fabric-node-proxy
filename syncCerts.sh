2b7bb4ec6f7ae0ca3172718d762746431aa1799a
Javier Aguirre <contacto@javaguirre.net>
2018-11-08 18:35:26 +0100

    babelll

--- syncCerts.sh
+++ syncCerts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-node syncCerts.js
+./node_modules/.bin/babel-node syncCerts.js
 
 if [ ! -d "hfc-key-store" ]; then
-    node enrollAdmin.js
-    node registerUser.js
-    node syncCerts.js
+    ./node_modules/.bin/babel-node enrollAdmin.js
+    ./node_modules/.bin/babel-node registerUser.js
+    ./node_modules/.bin/babel-node syncCerts.js
 fi
