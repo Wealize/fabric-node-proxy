37c8bec29494f38b4b7cc69fae458746a7f16718
Javier Aguirre <contacto@javaguirre.net>
2018-12-28 09:48:41 +0100

    Change syncCerts to use the new method

--- syncCerts.sh
+++ syncCerts.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 
-./node_modules/.bin/babel-node pullCreds.js
-
-if [ ! -d "hfc-key-store" ]; then
-    ./node_modules/.bin/babel-node enrollAdmin.js
-    ./node_modules/.bin/babel-node registerUser.js
-    ./node_modules/.bin/babel-node pushCreds.js
-fi
+./node_modules/.bin/babel-node syncCerts.js
