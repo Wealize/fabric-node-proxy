f04603c10d64f8cf7e7894387e5820511047b7f6
Javier Aguirre <contacto@javaguirre.net>
2019-11-19 15:09:30 +0100

    Open port to internet

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import app from "./app"
 
-const PORT = process.env.PORT || 3030
+const PORT = parseInt(process.env.PORT, 10) || 3030
+const HOST = process.env.HOST || "0.0.0.0"
 
-app.listen(PORT, () => {
+app.listen(PORT, HOST, () => {
   console.log("Server listening in port " + PORT)
 })
