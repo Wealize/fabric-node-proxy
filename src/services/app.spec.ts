eadaba48550d7392829f1b71ab92813e65d122cb
Javier Aguirre <contacto@javaguirre.net>
2019-11-19 13:10:36 +0100

    Added POST specs

--- src/services/app.spec.ts
+++ src/services/app.spec.ts
@@ -44,6 +44,14 @@ describe("GET chaincode", () => {
     expect(res.statusCode).toEqual(400)
   })
 
+  it("Get chaincode method should be bad request if invalid read method", async () => {
+    const res = await request(app)
+      .post("/api/v1/test/bad/writeChaincode?token=token")
+      .send()
+
+    expect(res.statusCode).toEqual(400)
+  })
+
   // TODO haven't figure it out yet
   // it("Get chaincode method should be ok if all valid", async () => {
   //   const mockedFabricService = FabricService as jest.Mock<FabricService>
@@ -51,6 +59,68 @@ describe("GET chaincode", () => {
 
   //   const res = await request(app)
   //     .get("/api/v1/test/chaincode/readChaincode?token=token")
+  //     .send()
+
+  //   expect(res.statusCode).toEqual(200)
+  // })
+})
+
+describe("POST chaincode", () => {
+  it("Post index should be forbidden if not token", async () => {
+    const res = await request(app)
+      .post("/")
+      .send()
+
+    expect(res.statusCode).toEqual(401)
+  })
+
+  it("Post index should be not found if token without chaincode parameters", async () => {
+    const res = await request(app)
+      .post("/?token=token")
+      .send()
+
+    expect(res.statusCode).toEqual(404)
+  })
+
+  it("Post chaincode method should be bad request if no token", async () => {
+    const res = await request(app)
+      .post("/api/v1/test/chaincode/hello")
+      .send()
+
+    expect(res.statusCode).toEqual(401)
+  })
+
+  it("Post chaincode method should be bad request if invalid channel", async () => {
+    const res = await request(app)
+      .post("/api/v1/bad/chaincode/readChaincode?token=token")
+      .send()
+
+    expect(res.statusCode).toEqual(400)
+  })
+
+  it("Post chaincode method should be bad request if invalid chaincode", async () => {
+    const res = await request(app)
+      .post("/api/v1/test/bad/readChaincode?token=token")
+      .send()
+
+    expect(res.statusCode).toEqual(400)
+  })
+
+  it("Post chaincode method should be bad request if invalid write method", async () => {
+    const res = await request(app)
+      .post("/api/v1/test/bad/readChaincode?token=token")
+      .send()
+
+    expect(res.statusCode).toEqual(400)
+  })
+
+  // TODO haven't figure it out yet
+  // it("Get chaincode method should be ok if all valid", async () => {
+  //   const mockedFabricService = FabricService as jest.Mock<FabricService>
+  //   mockedFabricService.mockImplementation()
+
+  //   const res = await request(app)
+  //     .post("/api/v1/test/chaincode/readChaincode?token=token")
   //     .send()
 
   //   expect(res.statusCode).toEqual(200)
